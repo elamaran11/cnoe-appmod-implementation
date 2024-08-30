e52a34ade548f7a1e41a1716350d6c1affa29335
Zach Jacobson <zjacobso@amazon.com>
2024-08-30 11:09:06 -0700

    get rid of extre exec

--- terraform/cert-manager.tf
+++ terraform/cert-manager.tf
@@ -2,12 +2,6 @@ resource "kubectl_manifest" "application_argocd_cert_manager" {
   yaml_body = templatefile("${path.module}/templates/argocd-apps/cert-manager.yaml", {
     REPO_URL = local.repo_url
   })
-
-  provisioner "local-exec" {
-    command = "kubectl wait --for=jsonpath=.status.health.status=Healthy -n argocd application/cert-manager && kubectl wait --for=jsonpath=.status.sync.status=Synced --timeout=300s -n argocd application/cert-manager && sleep 60"
-
-    interpreter = ["/bin/bash", "-c"]
-  }
 }
 
 resource "null_resource" "wait_for_cert_manager" {
