e679c394c065f47b874ec196c7cab8a7e8ec9d1e
Zach Jacobson <zjacobso@amazon.com>
2024-09-03 12:50:45 -0700

    switch to github

--- terraform/terraform-integrations.tf
+++ terraform/terraform-integrations.tf
@@ -1,9 +1,9 @@
 resource "kubectl_manifest" "flux-manifest" {
   count     = local.tf_integrations_count
-  yaml_body = file("${path.module}/templates/manifests/fluxcd.yaml")
+  yaml_body = file("https://raw.githubusercontent.com/cnoe-io/stacks/main/terraform-integrations/fluxcd.yaml")
 }
 
 resource "kubectl_manifest" "tofu-manifest" {
   count     = local.tf_integrations_count
-  yaml_body = file("${path.module}/templates/manifests/tofu-controller.yaml")
+  yaml_body = file("https://raw.githubusercontent.com/cnoe-io/stacks/main/terraform-integrations/tofu-controller.yaml")
 }
