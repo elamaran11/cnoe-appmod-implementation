9c7d26d09e99235efcb0b8bd9abccb9830ce9ff3
Zach Jacobson <zjacobso@amazon.com>
2024-09-03 12:39:39 -0700

    fix functions

--- terraform/terraform-integrations.tf
+++ terraform/terraform-integrations.tf
@@ -1,9 +1,9 @@
 resource "kubectl_manifest" "flux-manifest" {
   count     = local.tf_integrations_count
-  yaml_body = templatefile("${path.module}/templates/manifests/fluxcd.yaml")
+  yaml_body = file("${path.module}/templates/manifests/fluxcd.yaml")
 }
 
 resource "kubectl_manifest" "tofu-manifest" {
   count     = local.tf_integrations_count
-  yaml_body = templatefile("${path.module}/templates/manifests/tofu-controller.yaml")
+  yaml_body = file("${path.module}/templates/manifests/tofu-controller.yaml")
 }
