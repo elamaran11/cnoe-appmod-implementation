aed9d2942e192520560559062206640cea3f73e7
Zach Jacobson <zjacobso@amazon.com>
2024-09-03 12:32:06 -0700

    enable tf integrations work

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,22 @@
 
 locals {
-  repo_url = trimsuffix(var.repo_url, "/")
-  region = var.region
-  tags = var.tags
-  cluster_name    = var.cluster_name
-  hosted_zone_id = var.hosted_zone_id
-  dns_count = var.enable_dns_management ? 1 : 0
-  secret_count = var.enable_external_secret ? 1 : 0
+  repo_url              = trimsuffix(var.repo_url, "/")
+  region                = var.region
+  tags                  = var.tags
+  cluster_name          = var.cluster_name
+  hosted_zone_id        = var.hosted_zone_id
+  dns_count             = var.enable_dns_management ? 1 : 0
+  secret_count          = var.enable_external_secret ? 1 : 0
+  tf_integrations_count = var.enable_terraform_integrations ? 1 : 0
 
-  domain_name = var.enable_dns_management ? "${trimsuffix(data.aws_route53_zone.selected[0].name, ".")}" : "${var.domain_name}"
-  kc_domain_name = "keycloak.${local.domain_name}"
-  kc_cnoe_url = "https://${local.kc_domain_name}/realms/cnoe"
-  argo_domain_name = "argo.${local.domain_name}"
-  argo_redirect_url = "https://${local.argo_domain_name}/oauth2/callback"
-  argocd_domain_name = "argocd.${local.domain_name}"
+  domain_name           = var.enable_dns_management ? "${trimsuffix(data.aws_route53_zone.selected[0].name, ".")}" : "${var.domain_name}"
+  kc_domain_name        = "keycloak.${local.domain_name}"
+  kc_cnoe_url           = "https://${local.kc_domain_name}/realms/cnoe"
+  argo_domain_name      = "argo.${local.domain_name}"
+  argo_redirect_url     = "https://${local.argo_domain_name}/oauth2/callback"
+  argocd_domain_name    = "argocd.${local.domain_name}"
   backstage_domain_name = "backstage.${local.domain_name}"
-  gitea_domain_name = "gitea.${local.domain_name}"
+  gitea_domain_name     = "gitea.${local.domain_name}"
 }
 
 
@@ -27,7 +28,7 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
+  config_path = "~/.kube/config"
 }
 
 provider "helm" {
