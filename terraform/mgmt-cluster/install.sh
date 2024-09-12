996a24ea878e23622701e70d236d58c02c987ef6
hmuthusamy <hmmuthus@amazon.com>
2024-09-12 09:38:45 -0400

    Cleanup part1

--- terraform/mgmt-cluster/install.sh
+++ terraform/mgmt-cluster/install.sh
@@ -24,7 +24,7 @@ terraform apply -auto-approve
 
 kubectl apply -f ./karpenter.yaml
 
-export GITHUB_URL=$(yq '.repo_url' ./setups/config.yaml)
+export GITHUB_URL=$(yq '.repo_url' ../../setups/config.yaml)
 
 # Set up ArgoCD. We will use ArgoCD to install all components.
 cd "${REPO_ROOT}/setups/argocd/"
---
+++ terraform/setup-apps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e -o pipefail
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
