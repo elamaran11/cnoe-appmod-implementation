fe17d41405a690ed4afb38b43ec8eae53894db24
hmuthusamy <hmmuthus@amazon.com>
2024-09-13 14:20:51 -0400

    updated script and wildcard redirect

--- setup-oneshot.sh
+++ setup-oneshot.sh
@@ -9,14 +9,24 @@ ${REPO_ROOT}/terraform/mgmt-cluster/install.sh
 # Set the DNS_HOSTNAME to be checked
 export DNS_HOSTNAME=$(kubectl get service  ingress-nginx-controller -n ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 
-echo $DNS_HOSTNAME
-
 # Replace dns with the value of DNS_HOSTNAME
 sed -e "s/INGRESS_DNS/${DNS_HOSTNAME}/g" ${REPO_ROOT}/setups/default-config.yaml > ${REPO_ROOT}/setups/config.yaml
 
 # Deploy the apps
 
 ${REPO_ROOT}/setups/install.sh
+
+echo "ArgoCD URL is: http://$DNS_HOSTNAME/argocd"
+
+echo "GITEA URL is: http://$DNS_HOSTNAME/gitea"
+
+echo "Keycloak URL is: http://$DNS_HOSTNAME/keycloak"
+
+echo "Backstage URL is: http://$DNS_HOSTNAME/"
+
+echo "ArgoWorkflows URL is: http://$DNS_HOSTNAME/argo-workflows"
+
+
 
 # # Function to check if the dns is null or empty
 # check_dnshost() {
